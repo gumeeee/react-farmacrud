33525b137329eaa0e49ca1c572051424be7e7951
gumeeee <gaxco123@gmail.com>
2024-04-22 11:35:44 -0300

    Crud completo de categoria

---
+++ src/components/categorias/formularioCategorias/FormularioCategorias.tsx
@@ -0,0 +1,90 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { atualizar, buscar, cadastrar } from "../../../services/Service";
+import Categoria from "../../../models/Categoria";
+
+function FormularioCategorias() {
+  const [categoria, setCategoria] = useState<Categoria>({} as Categoria);
+
+  let navigate = useNavigate();
+
+  const { id } = useParams<{ id: string }>();
+
+  async function buscarPorId(id: string) {
+    await buscar(`/categorias/${id}`, setCategoria, {});
+  }
+
+  useEffect(() => {
+    if (id !== undefined) {
+      buscarPorId(id);
+    }
+  }, [id]);
+
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setCategoria({
+      ...categoria,
+      [e.target.name]: e.target.value,
+    });
+
+    console.log(JSON.stringify(categoria));
+  }
+
+  async function gerarNovaCategoria(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (id !== undefined) {
+      try {
+        await atualizar(`/categorias`, categoria, setCategoria, {});
+
+        alert("Categoria atualizado com sucesso");
+        retornar();
+      } catch (error: any) {
+        console.log(error);
+      }
+    } else {
+      try {
+        await cadastrar(`/categorias`, categoria, setCategoria, {});
+
+        alert("Categoria cadastrado com sucesso");
+      } catch (error: any) {
+        console.log(error);
+      }
+    }
+
+    retornar();
+  }
+
+  function retornar() {
+    navigate("/categorias");
+  }
+
+  return (
+    <div className="container flex flex-col items-center justify-center mx-auto">
+      <h1 className="text-4xl text-center my-8">
+        {id === undefined ? "Cadastre um novo tema" : "Editar tema"}
+      </h1>
+
+      <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovaCategoria}>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="descricao">Nome da categoria</label>
+          <input
+            type="text"
+            placeholder="Nome"
+            name="nome"
+            className="border-2 border-slate-700 rounded p-2"
+            value={categoria.nome}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+          />
+        </div>
+        <button
+          className="rounded text-slate-100 bg-indigo-400 hover:bg-indigo-800 w-1/2 py-2 mx-auto block"
+          type="submit"
+        >
+          {id === undefined ? "Cadastrar" : "Editar"}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default FormularioCategorias;
---
+++ src/components/categorias/listaCategorias/ListaCategorias.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Categoria from "../../../models/Categoria";
+import { buscar } from "../../../services/Service";
+import CardCategorias from "../cardCategorias/CardCategorias";
+import { DNA } from "react-loader-spinner";
+
+function ListaCategorias() {
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
+
+  let navigate = useNavigate();
+
+  async function buscarCategorias() {
+    try {
+      await buscar("/temas", setCategorias, {});
+    } catch (error: any) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    buscarCategorias();
+  }, [categorias.length]);
+
+  return (
+    <>
+      {categorias.length === 0 && (
+        <DNA
+          visible={true}
+          height="200"
+          width="200"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper mx-auto"
+        />
+      )}
+      <div className="flex justify-center w-full my-4">
+        <div className="container flex flex-col">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {categorias.map((categoria) => (
+              <>
+                <CardCategorias categoria={categoria} />
+              </>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default ListaCategorias;
